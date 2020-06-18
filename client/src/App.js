dc5996fd423d804231d5dbc28ab26c93c3455e4d
lucasestrade <43579718+lucasestrade@users.noreply.github.com>
2020-06-18 20:45:34 +0200

    routing react system

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+import { routes } from './providers/routes';
 
 function App() {
   return (
-    <div>Ca marche</div>
+    <Router>
+      {routes.map(({ path, Component }) => (
+        <Route exact
+          path={path} 
+          key={path} >
+            <Component />
+        </Route>
+      ))}
+    </Router>
   );
 }
 
---
+++ client/src/providers/routes.js
@@ -0,0 +1,13 @@
+import Home from '../components/Home';
+import Login from '../components/Login';
+
+export const routes = [
+    {
+        path: '/home',
+        Component: Home
+    },
+    {
+        path: '/login',
+        Component: Login
+    }
+]
