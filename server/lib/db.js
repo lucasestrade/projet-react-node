ce7fb43e018a864c8b24232bbce8ad325be0771b
Valeur Aucibi <valeuraucibi@gmail.com>
2020-07-14 11:52:37 +0200

    Synchronisation mongodb

--- server/lib/db.js
+++ server/lib/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    'mongodb://localhost:27017/mongoreact',
+    'mongodb://user:password@dbNode',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
